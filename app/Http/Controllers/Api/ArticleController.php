bde5355727baeebe787df0ab345f72174df79272
Faustino Vasquez Limon <faustino.vasquez@mitechnologiesinc.com>
2022-11-03 14:39:12 -0700

    fiters

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -17,8 +17,24 @@ class ArticleController extends Controller
      */
     public function index()
     {
+        $articles = Article::query();
 
-        $articles = Article::allowedSorts(['title','content']);
+        //filters
+        $allowedFilters = ['title','content','month','year'];
+
+        foreach(request('filter',[]) as $filter =>$value){
+            abort_unless(in_array($filter,$allowedFilters),400);
+
+            if ($filter === 'year'){
+                $articles->whereYear('created_at',$value);
+            }else if ($filter === 'month'){
+                $articles->whereMonth('created_at',$value);
+            }else{
+                $articles->where($filter,'LIKE','%'.$value.'%');
+            }
+        };
+
+        $articles->allowedSorts(['title','content']);
 
         return ArticleCollection::make($articles->jsonPaginate());
     }
